108b29b02012d79af0628cde2e2062ed8a1c77e5
Lue Andrie-Her <lue.her@gmail.com>
2019-05-07 17:49:13 -0400

    wip

--- tray_sensor/tools/tray_sensor_app.py
+++ tray_sensor/tools/tray_sensor_app.py
@@ -2,8 +2,9 @@ from tray_sensor import scanner
 
 def main():
     s = scanner.Scanner()
-    scan_entries = s.find_new_tags()
-    print(scan_entries)
+    s.find_new_tags()
+    print(s.tags)
+    import pdb;pdb.set_trace()
 
 if __name__ == "__main__":
     main()
