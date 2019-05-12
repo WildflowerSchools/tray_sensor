6996eea01d51bd51f84d4bfde20ceaa180b71c1f
Theodore Quinn <tcquinn@tcquinn.org>
2019-05-11 21:25:04 -0500

    Finish cleanup

--- task_list.md
+++ task_list.md
@@ -1,5 +1,3 @@
 # Task list
 
- * Add handling if no tags are found
- * Remove unused code
- * Clean up comments, docstrings, README
+ * TBD
--- tray_sensor/__init__.py
+++ tray_sensor/__init__.py
@@ -1,7 +1,3 @@
 """
 Provide functionality to communicate with Wildflower tray sensors via BLE
 """
-
-from tray_sensor.core import *
-# from shoe_sensor.databases.measurement_database import *
-# from shoe_sensor.databases.measurement_database.csv_local import *
